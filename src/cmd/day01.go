b982e844d95d27d07224342ce43b100861253fde
Abir Razzak <Abirrazzak1@gmail.com>
2022-12-03 12:11:18 -0500

    Day 01 (#1)

---
+++ src/cmd/day01.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2022 Abir Razzak Abirrazzak1@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"github.com/AbirRazzak/2022-advent-of-code/src/day01"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+)
+
+var part2Flag bool
+
+// day01Cmd represents the day01 command
+var day01Cmd = &cobra.Command{
+	Use:   "day01",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		logger := log.New(os.Stderr, "", log.LstdFlags)
+		runner := day01.CommandRunner{Logger: logger}
+
+		err := runner.Run(part2Flag)
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(day01Cmd)
+	day01Cmd.Flags().BoolVarP(&part2Flag, "part2", "2", false, "Run part 2 of the puzzle instead of part 1")
+}
